5f4146d6b05b061ad315472957b63ad2baf133b5
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2026-07-31 16:57:28 +0200

    logfile

--- collectTables.py
+++ collectTables.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-import glob
+import glob, time
 
 def clean ( line : str ) -> str:
     return line.replace("+","")
 
-def collect ( case : str = "Sleptons" ):
+def collect ( case : str = "Sleptons" ) -> int:
     path = f"tables/*/*-{case}_*.csv"
     files = glob.glob ( path )
     sm_case = "slep" if case == "Sleptons" else "chiwzoff"
@@ -13,8 +13,8 @@ def collect ( case : str = "Sleptons" ):
     files += glob.glob ( sm_path )
     outfile = f"{case}.csv"
     first = True
+    ct_l = 0
     with open ( outfile, "wt" ) as out:
-        ct_l = 0
         for fname in files:
             with open ( fname, "rt" ) as f:
                 lines = f.readlines()
@@ -28,11 +28,23 @@ def collect ( case : str = "Sleptons" ):
                     ct_l += 1
         out.close()
         print ( f"{outfile}: {ct_l} lines" )
+    return ct_l
+
+def log( n_lines : dict ):
+    with open ( f"collection.log", "wt" ) as f:
+        f.write ( "{\n" )
+        f.write ( f"    'time': {time.asctime()},\n" )
+        for k, v in n_lines.items():
+            f.write ( f"    '{k}': {v},\n" )
+        f.write ( "}\n" )
 
 def collectAll():
     cases = [ "Sleptons", "EWKinos" ]
+    n_lines = {}
     for case in cases:
-        collect ( case )
+        nl  = collect ( case )
+        n_lines[case]=nl
+    log ( n_lines )
 
 # tables/1911.12606-1-0/table-Sleptons_bkgonly-Sleptons_patchset-0.csv
 # tables/1911.12606-10-0/results-EWKinos_bkgonly-EWKinos_patchset-0.csv
