fa5f032cac06a2c98afc2622917aa55d943e29ac
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2026-08-03 12:22:27 +0200

    tables

--- collectTables.py
+++ collectTables.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import glob, time, shutil, os
+import glob, time, shutil, os, subprocess
 
 def clean ( line : str ) -> str:
     return line.replace("+","")
@@ -10,7 +10,7 @@ def collect ( case : str = "Sleptons" ) -> int:
     files = glob.glob ( path )
     sm_case = "slep" if case == "Sleptons" else "chiwzoff"
     sm_path = f"../smodels-utils/stats_ml/full_{sm_case}/T*.csv"
-    #files += glob.glob ( sm_path )
+    files_sms = glob.glob ( sm_path )
     outfile = f"{case}.csv"
     first = True
     ct_l = 0
@@ -18,12 +18,18 @@ def collect ( case : str = "Sleptons" ) -> int:
         for fname in files:
             with open ( fname, "rt" ) as f:
                 lines = f.readlines()
-                #if len(lines)>1:
-                #    print ( f"{fname}: {len(lines)-1} lines" )
                 if first:
                     out.write ( clean ( lines[0] ) )
                     first = False
                 for line in lines[1:]:
+                    tokens = line.split(",")
+                    out.write ( clean ( line ) )
+                    ct_l += 1
+        for fname in files_sms:
+            with open ( fname, "rt" ) as f:
+                lines = f.readlines()
+                for line in lines[1:-1]:
+                    tokens = line.split(",")
                     out.write ( clean ( line ) )
                     ct_l += 1
         out.close()
@@ -49,9 +55,18 @@ def collectAll():
         nl  = collect ( case )
         n_lines[case]=nl
     log ( n_lines )
+    
+def postprocess():
+    cmd = "cp EWKinos.csv Sleptons.csv ~/git/likelihoods/data/"
+    o = subprocess.getoutput ( cmd )
+    print ( f"{cmd}: {o}" )
+    cmd = "cd ~/git/likelihoods/data && python ./conv_csv_npy.py"
+    o = subprocess.getoutput ( cmd )
+    print ( f"{cmd}: {o}" )
 
 # tables/1911.12606-1-0/table-Sleptons_bkgonly-Sleptons_patchset-0.csv
 # tables/1911.12606-10-0/results-EWKinos_bkgonly-EWKinos_patchset-0.csv
 
 if __name__ == "__main__":
     collectAll()
+    postprocess()
